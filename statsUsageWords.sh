e104e81c96860ebd58914f499876a85d7fbc0f6a
FacundoMol <fjm2571@gmail.com>
2022-08-11 19:48:23 -0300

    esta el 4

--- statsUsageWords.sh
+++ statsUsageWords.sh
@@ -3,20 +3,27 @@
 
 touch usodepalabras.txt
 touch textodepurado1.txt
+touch usopalabrasfinal.txt
 ARCHIVO=$1
 
+for palabra in $(cat textoprueba.txt); do
 echo $palabra | tr -d [",",".","-",";","!","_","?","?","¿","(",")"] >> textodepurado1.txt
+done
+
+#echo $(cat textodepurado1.txt)
 
 for palabra in $(cat textodepurado1.txt);
 do
 	if [[ ${#palabra} -gt 3 ]];
 	then
-		#DEPURADA=$(echo $palabra | tr -d [",",".","-",";","!","_","?","¿","(",")"])
-		$(grep -o -i $palabra textodepurado1.txt | wc -l)  >> usodepalabras.txt
+		grep -o $palabra textodepurado1.txt | wc -l >> usodepalabras.txt
 	fi
 done
 
-cat usodepalabras.txt | sort -u -r | head -10
 
-rm usodepalabras.txt
-rm textodepurado1.txt
+#echo $(cat usodepalabras.txt)
+cat usodepalabras.txt | sort -r -u | head -10 | uniq >> usopalabrasfinal.txt
+echo $(cat usopalabrasfinal.txt)
+
+#rm usodepalabras.txt
+#rm textodepurado1.txt
